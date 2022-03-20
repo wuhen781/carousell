f93c6b370a19b0f5eb3103c73b770394672c592d
zhangxiaomin <zhangxiaomin@360jinrong.net>
2022-03-20 16:49:33 +0800

    build

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 go mod tidy
 go build main.go
+if [$? -ne 0]
+	then
+echo " build fails, you can also directly run the compiled binary(main)" 
+	else
 echo "build success,you can run by following methods:"
 echo "./main"
 echo "cat input.txt | ./main"
+ fi
